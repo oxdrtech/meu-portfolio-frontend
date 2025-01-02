47deb7dd36199fa62d47df24957ce69207ff5e84
DDR23 <roboreserva.andre@gmail.com>
2025-01-02 15:37:17 -0300

    feat: adicionando useDevice

---
+++ src/hooks/useDevices.tsx
@@ -0,0 +1,9 @@
+import { useMantineTheme } from "@mantine/core";
+import { useMediaQuery } from "@mantine/hooks";
+
+export default function useDevices() {
+  const theme = useMantineTheme();
+  const isDesktop = useMediaQuery(`(min-width: ${theme.breakpoints.lg})`);
+  const isMobile = useMediaQuery(`(max-width: ${theme.breakpoints.xs})`);
+  return { isDesktop, isMobile };
+}
