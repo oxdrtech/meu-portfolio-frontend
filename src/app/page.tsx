83af01c63aa80b60522f1b043ec091d988670016
omegatech-andre <omega.ti_ind@outlook.com>
2025-02-18 09:30:24 -0300

    chore: reajuste de codigo em page raiz

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,31 +1,22 @@
 "use client";
-import Background from "@/components/_ui/background/background";
 import { Stack } from "@mantine/core";
 import { useState } from "react";
 import Loading from "@/components/_ui/loading/loading";
-import LeftNavigation from "@/components/_ui/navigationBar/leftNavigation";
-import RightNavigation from "@/components/_ui/navigationBar/rightNavigation";
-import themeDevices from "@/styles/themeDevices";
 import PageSkills from "@/components/pages/skills/pageSkills";
 import PageCareers from "@/components/pages/careers/pageCareers";
 import PageHero from "@/components/pages/hero/pageHero";
 import PageContact from "@/components/pages/contact/pageContact";
-import CustomAffix from "@/components/_ui/affix/customAffix";
 
 export default function Home() {
-  const { isMobile } = themeDevices();
   const [renderCompleted, setRenderCompleted] = useState(false);
 
   return (
     <>
       <Stack>
         <Loading onComplete={() => setRenderCompleted(true)} />
-        <Background />
-        <LeftNavigation triggerGSAP={renderCompleted} />
-        <RightNavigation triggerGSAP={renderCompleted} />
-        <Stack display={"block"} className="main" h={renderCompleted ? "max-content" : "100vh"} style={{
-          backdropFilter: isMobile ? "blur(10px)" : "blur(16px)",
-          backgroundImage: 'url(./app.png)',
+        <Stack display={"block"} className="main" h={"100vh"} w={"100vw"} style={{
+          overflowY: "scroll",
+          scrollSnapType: "y mandatory",
         }}>
           {renderCompleted && (
             <>
@@ -36,7 +27,6 @@ export default function Home() {
             </>
           )}
         </Stack>
-        <CustomAffix />
       </Stack>
     </>
   );
