76c492a9522a625d620c8318a2d947ac115ec70b
omegatech-andre <omega.ti_ind@outlook.com>
2025-02-19 10:13:13 -0300

    save

--- src/components/pages/hero/pageHero.tsx
+++ src/components/pages/hero/pageHero.tsx
@@ -1,8 +1,7 @@
-import Background from "@/components/_ui/background/background";
-import TopNavigation from "@/components/_ui/navigationBar/topNavigation";
-import RightNavigation from "@/components/_ui/navigationBar/topNavigation";
+import { socialButtonsMock } from "@/mocks/navigationButtons.mock";
 import themeDevices from "@/styles/themeDevices";
-import { Avatar, BackgroundImage, Flex, Group, Image, Paper, Stack, Text } from "@mantine/core";
+import { ActionIcon, Avatar, BackgroundImage, Badge, Flex, Group, Paper, Stack, Text, Title } from "@mantine/core";
+import { useHover } from "@mantine/hooks";
 
 export default function PageHero() {
   const { isMobile } = themeDevices();
@@ -53,6 +52,18 @@ export default function PageHero() {
   //   };
   // }, { scope: gsapRef })
 
+  const socialButtons = socialButtonsMock.map((btn, index) => {
+    const { hovered, ref } = useHover();
+
+    return (
+      <Group key={index} component={"span"} style={{ overflow: "hidden" }}>
+        <ActionIcon ref={ref} component="a" href={btn.url} target="_blank" variant="transparent" fz={"sm"} c={hovered ? "defaultColor" : "#C9C9C9"} >
+          <btn.icon size={22} />
+        </ActionIcon>
+      </Group>
+    );
+  })
+
   return (
     <>
       <Flex className="panel" id="inicio" w={"100vw"} h={"100vh"} justify={"center"} direction={"column"} style={{
@@ -63,12 +74,59 @@ export default function PageHero() {
           overflow: "hidden",
         }}>
           <BackgroundImage src={"./backgroundHero.gif"}>
-            <Stack h={"100%"} style={{
-              backdropFilter: isMobile ? "blur(14px)" : "blur(10px)",
-              backgroundColor: "#11111180",
+            <Stack h={"100%"} p={"lg"} align={"center"} justify={"flex-end"} pb={"74"} gap={"xl"} style={{
+              backdropFilter: isMobile ? "blur(6px)" : "blur(6px)",
               backgroundImage: 'url(./noise.png)',
+              backgroundColor: "#11111160",
             }}>
-              teste
+              <Stack w={"80vw"} gap={"0"}>
+                <Group mb={"lg"} gap={"xs"}>
+                  <Badge variant={"outline"} color={"#C9c9c9"} size={"lg"} >Sistemas</Badge>
+                  <Badge variant={"outline"} color={"#C9c9c9"} size={"lg"} >Soluções</Badge>
+                </Group>
+                <Title order={1} fz={isMobile ? "h2" : ""} style={{
+                  textShadow: "-2px 2px 1px rgba(89, 112, 8, 0.50)",
+                }}>
+                  Desenvolvedor Web FullStack
+                </Title>
+                <Title order={1} fz={isMobile ? "h2" : ""} style={{
+                  textShadow: "-2px 2px 1px rgba(89, 112, 8, 0.50)",
+                }}>
+                  Criação de interfaces
+                </Title>
+              </Stack>
+              <Paper className="line-animated" w={"80vw"} h={2} bg={"#C9C9C9"} />
+              <Stack w={"80vw"} gap={"0"} ta={"end"}>
+                <Title order={2} fz={isMobile ? "h3" : ""} style={{
+                  textShadow: "-2px 2px 1px rgba(89, 112, 8, 0.50)",
+                }}>
+                  Criando soluções
+                </Title>
+                <Title order={2} fz={isMobile ? "h3" : ""} style={{
+                  textShadow: "-2px 2px 1px rgba(89, 112, 8, 0.50)",
+                }}>
+                  simples para problemas
+                </Title>
+                <Title order={2} fz={isMobile ? "h3" : ""} style={{
+                  textShadow: "-2px 2px 1px rgba(89, 112, 8, 0.50)",
+                }}>
+                  complexos
+                </Title>
+              </Stack>
+              <Paper className="line-animated" w={"80vw"} h={2} bg={"#C9C9C9"} />
+              <Group w={"80vw"} >
+                <Avatar className="avatar-animated" size={"60"} src={"https://avatars.githubusercontent.com/u/83263335?v=4"} />
+                <Stack gap={"0"}>
+                  <Text fz={"h4"} pl={"4"} style={{
+                    textShadow: "-2px 2px 1px rgba(89, 112, 8, 0.50)",
+                  }}>
+                    André Campos
+                  </Text>
+                  <Group gap={"xs"}>
+                    {socialButtons}
+                  </Group>
+                </Stack>
+              </Group>
             </Stack>
           </BackgroundImage>
         </Flex>
--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -6,6 +6,28 @@ export const theme = createTheme({
   primaryColor: 'defaultColor',
   fontFamily: 'Sulphur Point, serif',
   fontFamilyMonospace: 'Space Mono, monospace',
+  headings: {
+    sizes: {
+      h1: {
+        fontSize: rem(64),
+        lineHeight: "100%",
+        fontWeight: "lighter"
+      },
+      h2: {
+        fontSize: rem(40),
+        lineHeight: "100%",
+        fontWeight: "lighter"
+      },
+      h3: {
+        fontSize: rem(24),
+        lineHeight: "100%",
+        fontWeight: "lighter"
+      },
+      h4: {
+        fontSize: rem(18),
+      },
+    }
+  },
   fontSizes: {
     xs: rem(12),
     sm: rem(14),
