f8674834f5827bbdcbe03c47f2774a2b3121662b
omegatech-andre <omega.ti_ind@outlook.com>
2025-02-19 15:31:25 -0300

    chore: ajustes de pageCareers

--- src/components/pages/careers/pageCareers.tsx
+++ src/components/pages/careers/pageCareers.tsx
@@ -1,13 +1,12 @@
-import TopNavigation from "@/components/_ui/navigationBar/topNavigation";
 import { careersMock } from "@/mocks/careers.mock";
 import themeDevices from "@/styles/themeDevices";
 import { formatDate } from "@/utils/formatDate";
-import { Accordion, Avatar, Card, Flex, Group, Highlight, Stack, Text } from "@mantine/core";
+import { Accordion, Avatar, Flex, Group, Highlight, Paper, Stack, Text } from "@mantine/core";
 import { IconArrowDownLeft, IconBriefcaseFilled } from "@tabler/icons-react";
 import { useState } from "react";
 
 export default function PageCareers() {
-  const { isMobile } = themeDevices();
+  const { isMobile, isDesktop } = themeDevices();
   const [openedItem, setOpenedItem] = useState<string | null>(careersMock[0]?.id || null);
 
   const careers = careersMock.map((career, index) => (
@@ -18,37 +17,40 @@ export default function PageCareers() {
       onMouseLeave={() => setOpenedItem(careersMock[0]?.id)}
     >
       <Accordion.Control pos={"relative"}>
-        <Stack gap={"xs"} pt={openedItem === career.id ? (isMobile ? "" : "lg") : ""} style={{
-          transition: "0.4s ease",
-        }}>
-          <Flex gap={"md"} align={"center"}>
-            <Avatar
-              src={career.CAREER_COMPANY_LOGO && career.CAREER_COMPANY_LOGO}
-              size={"50"} radius={"sm"}>
-              {!career.CAREER_COMPANY_LOGO && <IconBriefcaseFilled color="#DAFF01" />}
-            </Avatar>
-            <Stack gap={"4"}>
-              <Text fw={"bold"} fz={"lg"} c={"defaultColor"} inline style={{
-                textShadow: "-2px 2px 1px rgba(89, 112, 8, 0.50)",
-              }}>{career.CAREER_NAME}</Text>
-              <Text fw={"bold"} inline style={{
-                textShadow: "-2px 2px 1px rgba(89, 112, 8, 0.50)",
-              }}>{career.CAREER_COMPANY}</Text>
-              {
-                career.CAREER_START_DATE
-                && (
-                  <Stack gap={"4"}>
-                    <Group gap={"6"}>
-                      <Text fz={"xs"} c={"dimmed"} inline>{formatDate(career.CAREER_START_DATE)}</Text>
-                      <Text fz={"xs"} c={"dimmed"} inline>-</Text>
-                      <Text fz={"xs"} c={"dimmed"} inline>{career.CAREER_END_DATE ? formatDate(career.CAREER_END_DATE) : "o momento"}</Text>
-                    </Group>
-                  </Stack>
-                )
-              }
-            </Stack>
-          </Flex>
-        </Stack>
+        <Flex gap={"md"} align={"center"}>
+          <Avatar
+            src={career.CAREER_COMPANY_LOGO && career.CAREER_COMPANY_LOGO}
+            size={"50"}
+            radius={"sm"}
+          >
+            {
+              !career.CAREER_COMPANY_LOGO
+              && (
+                <IconBriefcaseFilled color="#DAFF01" />
+              )
+            }
+          </Avatar>
+          <Stack gap={"4"}>
+            <Text fw={"bold"} c={"defaultColor"} inline>
+              {career.CAREER_NAME}
+            </Text>
+            <Text fw={"bold"} inline>
+              {career.CAREER_COMPANY}
+            </Text>
+            {
+              career.CAREER_START_DATE
+              && (
+                <Stack gap={"4"}>
+                  <Group gap={"6"}>
+                    <Text fz={"xs"} c={"dimmed"} inline>{formatDate(career.CAREER_START_DATE)}</Text>
+                    <Text fz={"xs"} c={"dimmed"} inline>-</Text>
+                    <Text fz={"xs"} c={"dimmed"} inline>{career.CAREER_END_DATE ? formatDate(career.CAREER_END_DATE) : "o momento"}</Text>
+                  </Group>
+                </Stack>
+              )
+            }
+          </Stack>
+        </Flex>
         <Stack pos={"absolute"} right={"10px"} top={"10px"}>
           <IconArrowDownLeft size={"28"} color="grey" style={{
             rotate: openedItem === career.id ? "180deg" : "0deg",
@@ -57,50 +59,44 @@ export default function PageCareers() {
         </Stack>
       </Accordion.Control>
       <Accordion.Panel>
-        <Stack px={"sm"} pb={openedItem === career.id ? (isMobile ? "" : "lg") : ""} style={{
-          transition: "0.4s ease",
-        }}>
-          <Text fz={"sm"} inline>
-            {career.CAREER_DESCRIPTION}
-          </Text>
-        </Stack>
+        <Text fz={"sm"} px={isMobile ? "" : "sm"} inline>
+          {career.CAREER_DESCRIPTION}
+        </Text>
       </Accordion.Panel>
     </Accordion.Item>
   ));
 
   return (
     <>
-      <Flex className="panel" id="sobre-mim" h={"100vh"} justify={"center"} align={"center"} style={{
+      <Flex className="panel" id="sobre-mim" w={"100vw"} h={"100vh"} justify={"center"} direction={"column"} style={{
         scrollSnapAlign: "start",
       }}>
-        <Stack h={"90vh"} w={"70rem"} maw={"90vw"} align={"center"}>
-          <Stack>
-            <Highlight
-              highlight={[
-                "qualidade",
-                "performance",
-                "design",
-                "experiência",
-              ]}
-              highlightStyles={{
-                color: "#DAFF01",
-                WebkitBackgroundClip: 'text',
-              }}
-              ta={"center"}
-              fw={"bold"}
-              fz={isMobile ? "3vh" : "5vh"}
-              inline
-              style={{
-                textShadow: "-2px 2px 1px rgba(89, 112, 8, 0.50)",
-              }}>
-              Há 2 anos desenvolvendo aplicações web sob medida para empresas e negócios independentes, criando soluções reais que unem qualidade, alta performance, design elegante e uma experiência do usuário marcante
-            </Highlight>
-          </Stack>
-          <Stack flex={"1"} justify={"center"} w={"60rem"} maw={"90vw"}>
-            <Card p={"0"} radius={"md"} style={{
-              backdropFilter: "blur(100px)",
-              background: "#23232350",
-            }}>
+        <Flex m={"56 10 10 10"} flex={"1"} justify={"center"} bg={"dark"} style={{
+          borderRadius: "16px",
+          overflow: "hidden",
+        }}>
+          <Stack h={"100%"} p={"lg"} align={"center"} justify={"center"} gap={"xl"}>
+            <Stack w={isMobile ? "90vw" : "80vw"} gap={"0"}>
+              <Highlight
+                highlight={[
+                  "qualidade",
+                  "performance",
+                  "design",
+                  "experiência",
+                ]}
+                highlightStyles={{
+                  color: "#DAFF01",
+                  WebkitBackgroundClip: 'text',
+                }}
+                fz={isMobile ? "h3" : !isDesktop ? "h2" : "h1"}
+                fw={"lighter"}
+                inline
+              >
+                Há 2 anos desenvolvendo aplicações web para empresas e negócios independentes, unindo qualidade, alta performance, design elegante e uma experiência do usuário marcante
+              </Highlight>
+            </Stack>
+            <Paper w={isMobile ? "90vw" : "80vw"} h={1} bg={"defaultColor"} />
+            <Stack w={isMobile ? "90vw" : "80vw"}>
               <Accordion
                 value={openedItem}
                 variant={"contained"}
@@ -109,9 +105,9 @@ export default function PageCareers() {
               >
                 {careers}
               </Accordion>
-            </Card>
+            </Stack>
           </Stack>
-        </Stack>
+        </Flex>
       </Flex>
     </>
   );
