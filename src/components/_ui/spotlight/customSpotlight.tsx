987d61cbc04a260ab0d4b4fe8a3b147ca05eab4b
ddr23 <roboreserva.andre@gmail.com>
2025-02-24 16:15:06 -0300

    chore: ajustes de css em spotlight

--- src/components/_ui/spotlight/customSpotlight.tsx
+++ src/components/_ui/spotlight/customSpotlight.tsx
@@ -1,15 +1,16 @@
 import { useActions } from "@/mocks/actions.mock";
 import { Spotlight } from "@mantine/spotlight";
 import { IconSearch } from "@tabler/icons-react";
+import classes from "./customSpotlight.module.css"
 
 export default function CustomSpotlight() {
   const actions = useActions();
 
   return (
     <Spotlight
+      classNames={classes}
       actions={actions}
       nothingFound="Não encontrado..."
-      highlightQuery
       shortcut="ctrl+k"
       searchProps={{
         leftSection: <IconSearch size={22} />,
