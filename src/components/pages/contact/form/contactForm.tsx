7428e483c71f883db9c50f9deb43f2519d6c7a62
ddr23 <roboreserva.andre@gmail.com>
2025-03-10 17:31:25 -0300

    save

--- src/components/pages/contact/form/contactForm.tsx
+++ src/components/pages/contact/form/contactForm.tsx
@@ -6,9 +6,10 @@ import { Contact } from "@/types/contact";
 import { API_BASE_URL } from "@/utils/apiBaseUrl";
 import { yupResolver } from "@hookform/resolvers/yup";
 import { Button, SimpleGrid, Stack, Text, Textarea, TextInput, Title } from "@mantine/core";
-import { IconCircleCheck } from "@tabler/icons-react";
 import { useEffect } from "react";
 import { Controller, useForm } from "react-hook-form";
+
+// TODO - trabalhar aqui
 
 export default function ContactForm() {
   const { isMobile } = themeDevices();
@@ -18,7 +19,7 @@ export default function ContactForm() {
   });
 
   const watchData = watch();
-  const { isPosting, response, error, sendRequest } = usePost<Contact, Contact>(`${API_BASE_URL}/contacts/create`, watchData);
+  const { isPosting, response, error, sendRequest } = usePost<Contact, { contact: Contact, discordStatus: number }>(`${API_BASE_URL}/contacts/create`, watchData);
 
   useEffect(() => {
     if (error) {
@@ -36,13 +37,13 @@ export default function ContactForm() {
     }
   }, [response, error]);
 
-  if (response) {
+  if (!response) {
     return (
-      <Stack align="center" gap={0}>
-        <IconCircleCheck color="#DAFF01" size={100} stroke={"1"} />
-        <Stack gap={"xs"}>
-          <Text ta='center' c='dimmed'>Formulario enviado com sucesso</Text>
-          <Button fullWidth variant="light">Enviar um novo formulário</Button>
+      <Stack align="center" gap={"md"} justify={"center"}>
+        <Text ta='center' c='defaultColor'>Formulario enviado</Text>
+        <Stack gap={"6"}>
+          <Text ta='center' c='dimmed' fz={"sm"} inline>Seu formulário foi enviado para análise</Text>
+          <Text ta='center' c='dimmed' fz={"sm"} inline>Tempo estimado do contato é de 24h</Text>
         </Stack>
       </Stack>
     );
@@ -50,79 +51,76 @@ export default function ContactForm() {
 
   return (
     <>
-      <Stack gap={"xs"} mb={"md"}>
-        {
-          !isMobile
-          && (
-            <Title order={2} c={"defaultColor"} style={{
-              textShadow: "-2px 2px 1px rgba(89, 112, 8, 0.50)",
-            }}>
-              Tem um projeto empolgante e precisa de ajuda?
-            </Title>
-          )
-        }
-        <Title order={3} fz={isMobile ? "h4" : ""} c={"defaultColor"} style={{
-          textShadow: "-2px 2px 1px rgba(89, 112, 8, 0.50)",
-        }}>
-          Preenche o formulário abaixo com nome e whatsapp que eu entro em contato.
-        </Title>
-      </Stack>
-      <form onSubmit={handleSubmit(sendRequest)}>
-        <Stack gap="lg">
-          <SimpleGrid cols={{ base: 1, sm: 1 }}>
-            <SimpleGrid cols={{ base: 1, sm: 2 }}>
+      <Stack w={"80vw"}>
+        <Stack gap={"xs"} mb={"md"}>
+          <Title order={2} c={"defaultColor"} style={{
+            textShadow: "-2px 2px 1px rgba(89, 112, 8, 0.50)",
+          }}>
+            Tem um projeto empolgante e precisa de ajuda?
+          </Title>
+          <Title order={3} fz={isMobile ? "h4" : ""} c={"defaultColor"} style={{
+            textShadow: "-2px 2px 1px rgba(89, 112, 8, 0.50)",
+          }}>
+            Preenche o formulário abaixo com nome e whatsapp que eu entro em contato.
+          </Title>
+        </Stack>
+        <form onSubmit={handleSubmit(sendRequest)}>
+          <Stack gap="lg">
+            <SimpleGrid cols={{ base: 1, sm: 1 }}>
+              <SimpleGrid cols={{ base: 1, sm: 2 }}>
+                <Controller
+                  name="name"
+                  control={control}
+                  render={({ field }) => (
+                    <TextInput
+                      {...field}
+                      placeholder="Nome*"
+                      required
+                      value={field.value || ""}
+                      onChange={(value) => field.onChange(value || "")}
+                    />
+                  )}
+                />
+                <Controller
+                  name="phoneNumber"
+                  control={control}
+                  render={({ field }) => (
+                    <TextInput
+                      {...field}
+                      placeholder="Whatsapp*"
+                      required
+                      value={field.value || ""}
+                      onChange={(value) => field.onChange(value || "")}
+                    />
+                  )}
+                />
+              </SimpleGrid>
               <Controller
-                name="contact_name"
+                name="projectDescription"
                 control={control}
                 render={({ field }) => (
-                  <TextInput
+                  <Textarea
                     {...field}
-                    placeholder="Nome*"
-                    required
-                    value={field.value || ""}
-                    onChange={(value) => field.onChange(value || "")}
-                  />
-                )}
-              />
-              <Controller
-                name="contact_phoneNumber"
-                control={control}
-                render={({ field }) => (
-                  <TextInput
-                    {...field}
-                    placeholder="Whatsapp*"
-                    required
+                    placeholder="Me fala um pouco do teu projeto"
+                    minRows={2}
+                    autosize
                     value={field.value || ""}
                     onChange={(value) => field.onChange(value || "")}
                   />
                 )}
               />
             </SimpleGrid>
-            <Controller
-              name="contact_projectDescription"
-              control={control}
-              render={({ field }) => (
-                <Textarea
-                  {...field}
-                  placeholder="Me fala um pouco do teu projeto"
-                  minRows={2}
-                  autosize
-                  value={field.value || ""}
-                  onChange={(value) => field.onChange(value || "")}
-                />
-              )}
-            />
-          </SimpleGrid>
-          <Button
-            type={"submit"}
-            variant={"light"}
-            fullWidth
-            loading={isPosting}
-          >
-            Enviar formulário
-          </Button>
-        </Stack>
-      </form>
+            <Button
+              type={"submit"}
+              variant={"light"}
+              fullWidth
+              loading={isPosting}
+            >
+              Enviar formulário
+            </Button>
+          </Stack>
+        </form>
+      </Stack>
     </>
   );
 }
--- src/components/pages/contact/pageContact.tsx
+++ src/components/pages/contact/pageContact.tsx
@@ -1,12 +1,10 @@
 import themeDevices from "@/styles/themeDevices";
-import { Avatar, BackgroundImage, Button, Flex, Modal, Paper, Stack, Text, Title } from "@mantine/core";
-import { useDisclosure } from "@mantine/hooks";
-import ContactForm from "./form/contactForm";
+import { Avatar, BackgroundImage, Flex, Paper, Stack, Text, Title } from "@mantine/core";
 import SocialButtons from "@/components/_ui/socialButtons/socialButtons";
+import ContactForm from "./form/contactForm";
 
 export default function PageContact() {
   const { isMobile } = themeDevices();
-  const [opened, { open, close }] = useDisclosure(false);
 
   return (
     <>
@@ -27,27 +25,7 @@ export default function PageContact() {
                 </Title>
               </Stack>
               <Paper w={isMobile ? "90vw" : "80vw"} h={1} bg={"#C9C9C9"} />
-              <Stack w={isMobile ? "90vw" : "80vw"} gap={isMobile ? "" : "0"}>
-                {
-                  isMobile
-                    ? (
-                      <>
-                        <Title order={3} fz={"h4"} c={"defaultColor"} style={{
-                          textShadow: "-2px 2px 1px rgba(89, 112, 8, 0.50)",
-                        }}>
-                          Tem um projeto empolgante e precisa de ajuda?
-                        </Title>
-                        <Button variant="light" onClick={open}>
-                          {isMobile
-                            ? "Preencer formulário"
-                            : "Entrar em contato"
-                          }
-                        </Button>
-                      </>
-                    )
-                    : <ContactForm />
-                }
-              </Stack>
+              <ContactForm />
               <Stack w={"80vw"} align={"center"} ta={"center"} mt={"md"} gap={"xs"}>
                 <Avatar src={"https://avatars.githubusercontent.com/u/83263335?v=4"} size={"50"} />
                 <Stack align={"center"} gap={"0"}>
@@ -63,24 +41,6 @@ export default function PageContact() {
           </BackgroundImage>
         </Flex>
       </Flex>
-      <Modal
-        opened={opened}
-        onClose={close}
-        closeOnClickOutside={false}
-        transitionProps={{
-          transition: "scale",
-          duration: 200,
-          timingFunction: "easy",
-        }}
-        overlayProps={{
-          backgroundOpacity: 0.55,
-          blur: 3
-        }}
-        style={{
-          zIndex: 1000,
-        }}>
-        <ContactForm />
-      </Modal>
     </>
   );
 }
