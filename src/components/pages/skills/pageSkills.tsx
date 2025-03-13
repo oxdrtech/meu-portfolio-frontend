9975a51c1a9f7dae7c53e463fcf27a5d91d6ae50
ddr23 <roboreserva.andre@gmail.com>
2025-03-13 17:14:37 -0300

    save

--- src/components/pages/skills/pageSkills.tsx
+++ src/components/pages/skills/pageSkills.tsx
@@ -2,6 +2,9 @@ import themeDevices from "@/styles/themeDevices";
 import { Badge, Flex, Group, Highlight, Image, List, Paper, Stack, Title } from "@mantine/core";
 import { iconTechs } from "@/mocks/iconTechs.mock";
 import classes from "./pageSkill.module.css";
+import { useGSAP } from "@gsap/react";
+import { useRef } from "react";
+import gsap from "gsap";
 
 interface Props {
   triggerGSAP: boolean;
@@ -9,71 +12,128 @@ interface Props {
 
 export default function PageSkills({ triggerGSAP }: Props) {
   const { isMobile, isDesktop } = themeDevices();
+  const gsapRef = useRef(null);
+  const listRef = useRef(null); // Ref para os itens da lista
+
+  useGSAP(() => {
+    if (triggerGSAP) {
+      gsap.set(".containerSkills", {
+        display: "flex",
+        yPercent: -100,
+        opacity: 0,
+      });
+      gsap.set(".objectSkills", {
+        display: "flex",
+        yPercent: 100,
+        opacity: 0,
+      });
+
+      gsap.timeline()
+        .to(".containerSkills", {
+          delay: .1,
+          opacity: 1,
+          yPercent: 0,
+          duration: 0.5,
+        })
+        .to(".objectSkills", {
+          opacity: 1,
+          yPercent: 0,
+          duration: 0.5,
+        });
+
+      // Animar os itens da lista com GSAP (integrando com animação CSS)
+      gsap.fromTo(
+        ".root li",
+        {
+          opacity: 0,
+          y: 0,
+        },
+        {
+          opacity: 0.5,
+          y: -1000,
+          rotation: 720,
+          duration: 20,
+          repeat: -1,  // Repeate indefinido
+          ease: "linear", // Usar linear para a animação contínua
+        }
+      );
+    } else {
+      gsap.to(".containerSkills", {
+        opacity: 0,
+        duration: 0.25,
+        display: "none",
+      });
+    }
+  }, [triggerGSAP]);
 
   const animationTechs = iconTechs.map((icon, index) => (
     <li key={index}>
       <Image src={icon.src} alt="techs" />
     </li>
-  ))
+  ));
 
   return (
     <>
-      <Flex w={"100vw"} h={"100vh"} justify={"center"} direction={"column"} style={{
+      <Flex ref={gsapRef} w={"100vw"} h={"100vh"} justify={"center"} direction={"column"} style={{
         scrollSnapAlign: "start",
       }}>
-        <Flex m={"56 10 10 10"} pos={"relative"} flex={"1"} justify={"center"} bg={"#11111150"} style={{
+        <Flex className={"containerSkills"} display={"none"} m={"56 10 10 10"} pos={"relative"} flex={"1"} justify={"center"} bg={"#111111"} style={{
           borderRadius: "16px",
           overflow: "hidden",
         }}>
-          <List classNames={classes}>
+          <List classNames={classes} ref={listRef}>
             {animationTechs}
           </List>
           <Stack h={"100%"} px={"lg"} align={"center"} justify={isDesktop ? "flex-end" : "center"} pb={isDesktop ? "80" : ""} gap={isMobile ? "lg" : "xl"} style={{
             backdropFilter: "blur(1px)",
           }}>
-            <Stack w={isMobile ? "90vw" : "80vw"} gap={"lg"}>
-              <Group gap={"xs"}>
-                <Badge variant={"outline"}>Front-end</Badge>
-                <Badge variant={"outline"}>Back-end</Badge>
-              </Group>
-              <Highlight
-                highlight={[
-                  "modernas",
-                  "sólida",
-                  "evolução",
-                ]}
-                highlightStyles={{
-                  color: "#DAFF01",
-                  WebkitBackgroundClip: 'text',
-                }}
-                fz={isMobile ? "h2" : "h1"}
-                fw={"lighter"}
-                inline
-                style={{
+            <Group component={"span"} gap={"sm"} style={{
+              overflow: "hidden",
+            }}>
+              <Stack className={"objectSkills"} display={"none"} w={isMobile ? "90vw" : "80vw"} gap={"lg"}>
+                <Group gap={"xs"}>
+                  <Badge variant={"outline"}>Front-end</Badge>
+                  <Badge variant={"outline"}>Back-end</Badge>
+                </Group>
+                <Highlight
+                  highlight={["modernas", "sólida", "evolução"]}
+                  highlightStyles={{
+                    color: "#DAFF01",
+                    WebkitBackgroundClip: 'text',
+                  }}
+                  fz={isMobile ? "h2" : "h1"}
+                  fw={"lighter"}
+                  inline
+                  style={{
+                    textShadow: "-2px 2px 1px rgba(89, 112, 8, 0.50)",
+                  }}
+                >
+                  Com ferramentas modernas, cada projeto ganha uma base sólida, preparada para uma evolução contínua
+                </Highlight>
+              </Stack>
+            </Group>
+            <Paper className={"objectSkills"} display={"none"} w={isMobile ? "90vw" : "80vw"} h={1} bg={"defaultColor"} />
+            <Group component={"span"} gap={"sm"} style={{
+              overflow: "hidden",
+            }}>
+              <Stack className={"objectSkills"} display={"none"} w={isMobile ? "90vw" : "80vw"} gap={"0"} ta={"end"}>
+                <Title order={2} fz={isMobile ? "h3" : ""} style={{
                   textShadow: "-2px 2px 1px rgba(89, 112, 8, 0.50)",
-                }}
-              >
-                Com ferramentas modernas, cada projeto ganha uma base sólida, preparada para uma evolução contínua
-              </Highlight>
-            </Stack>
-            <Paper w={isMobile ? "90vw" : "80vw"} h={1} bg={"defaultColor"} />
-            <Stack w={isMobile ? "90vw" : "80vw"} gap={"0"} ta={"end"}>
-              <Title order={2} fz={isMobile ? "h3" : ""} style={{
-                textShadow: "-2px 2px 1px rgba(89, 112, 8, 0.50)",
-              }}>
-                Criando soluções reais
-              </Title>
-              <Title order={2} fz={isMobile ? "h3" : ""} style={{
-                textShadow: "-2px 2px 1px rgba(89, 112, 8, 0.50)",
-              }}>
-                que impactam
-              </Title>
-              <Title order={2} fz={isMobile ? "h3" : ""} style={{
-                textShadow: "-2px 2px 1px rgba(89, 112, 8, 0.50)",
-              }}>
-                os negócios
-              </Title>
-            </Stack>
+                }}>
+                  Criando soluções reais
+                </Title>
+                <Title order={2} fz={isMobile ? "h3" : ""} style={{
+                  textShadow: "-2px 2px 1px rgba(89, 112, 8, 0.50)",
+                }}>
+                  que impactam
+                </Title>
+                <Title order={2} fz={isMobile ? "h3" : ""} style={{
+                  textShadow: "-2px 2px 1px rgba(89, 112, 8, 0.50)",
+                }}>
+                  os negócios
+                </Title>
+              </Stack>
+            </Group>
           </Stack>
         </Flex>
       </Flex>
